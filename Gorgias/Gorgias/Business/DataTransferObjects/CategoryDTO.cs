91425193703422b4ee1d347001b1d6a2a7bdc0f2
odenza <tech@yellowtbs.com>
2017-08-18 17:40:32 +0800

    testing get and set for multi languages ;)
    testing get and set for multi languages ;)

--- Gorgias/Gorgias/Business/DataTransferObjects/CategoryDTO.cs
+++ Gorgias/Gorgias/Business/DataTransferObjects/CategoryDTO.cs
@@ -2,19 +2,42 @@
 using FluentValidation.Attributes;
 using Gorgias.Business.Validators;
 namespace Gorgias.Business.DataTransferObjects
-{   
+{
 
     [Validator(typeof(CategoryValidator))]
     public class CategoryDTO
     {
-            public int CategoryID{get; set;}
-            public String CategoryName{get; set;}
-            public Boolean CategoryStatus{get; set;}
-            public String CategoryImage{get; set;}
-            public String CategoryDescription{get; set;}
-            public int CategoryParentID{get; set;}
-        
-        
+        private string _categoryName;
+        public int CategoryID { get; set; }
+        public String CategoryName
+        {
+            get
+            {
+                if (Multilanguage == null)
+                {
+                    return Multilanguage;
+                }
+                return _categoryName;
+            }
+            set
+            {
+                _categoryName = value;
+            }
+        }
+        public Boolean CategoryStatus { get; set; }
+        public String CategoryImage { get; set; }
+        public String CategoryDescription { get; set; }
+        public int CategoryParentID { get; set; }
+
+        public string Multilanguage
+        {
+            get
+            {
+                return "Hello World ;)";
+            }
+        }
+
+
     }
-}   
+}
 
